68efd63bf643260c3b8f49a11cd6d0ddb60e7594
ColeTrammer <cole.trammer@gmail.com>
2024-04-27 16:31:55 -0700

    diusaudio: parse and play a simple 16 bit WAV file

--- include/dius/main.h
+++ include/dius/main.h
@@ -5,34 +5,35 @@
 #include <di/container/vector/prelude.h>
 #include <dius/print.h>
 
-#define DIUS_MAIN(Type, NS)                                              \
-    int main(int argc, char** argv) {                                    \
-        auto args = di::Vector<di::TransparentStringView> {};            \
-        for (int i = 0; i < argc; i++) {                                 \
-            char* arg = argv[i];                                         \
-            size_t len = 0;                                              \
-            while (arg[len] != '\0') {                                   \
-                len++;                                                   \
-            }                                                            \
-            args.push_back({ arg, len });                                \
-        }                                                                \
-                                                                         \
-        auto as_span = args.span();                                      \
-        auto parser = di::get_cli_parser<Type>();                        \
-        auto result = parser.parse(as_span);                             \
-        if (!result) {                                                   \
-            dius::eprintln("Failed to parse command line arguments"_sv); \
-            return 2;                                                    \
-        }                                                                \
-                                                                         \
-        using Result = decltype(NS::main(*result));                      \
-        if constexpr (di::concepts::Expected<Result>) {                  \
-            auto main_result = NS::main(*result);                        \
-            if (!main_result) {                                          \
-                return 1;                                                \
-            }                                                            \
-        } else {                                                         \
-            (void) NS::main(*result);                                    \
-        }                                                                \
-        return 0;                                                        \
+#define DIUS_MAIN(Type, NS)                                                                    \
+    int main(int argc, char** argv) {                                                          \
+        auto args = di::Vector<di::TransparentStringView> {};                                  \
+        for (int i = 0; i < argc; i++) {                                                       \
+            char* arg = argv[i];                                                               \
+            size_t len = 0;                                                                    \
+            while (arg[len] != '\0') {                                                         \
+                len++;                                                                         \
+            }                                                                                  \
+            args.push_back({ arg, len });                                                      \
+        }                                                                                      \
+                                                                                               \
+        auto as_span = args.span();                                                            \
+        auto parser = di::get_cli_parser<Type>();                                              \
+        auto result = parser.parse(as_span);                                                   \
+        if (!result) {                                                                         \
+            dius::eprintln("Failed to parse command line arguments"_sv);                       \
+            return 2;                                                                          \
+        }                                                                                      \
+                                                                                               \
+        using Result = decltype(NS::main(*result));                                            \
+        if constexpr (di::concepts::Expected<Result>) {                                        \
+            auto main_result = NS::main(*result);                                              \
+            if (!main_result) {                                                                \
+                dius::eprintln("Command failed: error: {}"_sv, main_result.error().message()); \
+                return 1;                                                                      \
+            }                                                                                  \
+        } else {                                                                               \
+            (void) NS::main(*result);                                                          \
+        }                                                                                      \
+        return 0;                                                                              \
     }
