52a06592c897bd60dd33f83734100596fbb02b0a
ColeTrammer <cole.trammer@gmail.com>
2024-05-18 17:20:56 -0700

    di: work around GCC bug with undefined behavior sanitizer

--- include/dius/test/prelude.h
+++ include/dius/test/prelude.h
@@ -2,8 +2,3 @@
 
 #include <di/assert/prelude.h>
 #include <dius/test/macro.h>
-
-#define TEST        DIUS_TEST
-#define TESTC       DIUS_TESTC
-#define TESTC_CLANG DIUS_TESTC_CLANG
-#define TESTC_GCC   DIUS_TESTC_GCC
