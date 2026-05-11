689ee7643eea8e45e50f980317408223aaa1a568
coletrammer <cole.trammer@gmail.com>
2026-05-10 23:01:29 -0700

    meta: support aarch64

--- include/platform/linux/dius/system/system_call.h
+++ include/platform/linux/dius/system/system_call.h
@@ -22,7 +22,9 @@ enum class Number : int {
     getdents64 = __NR_getdents64,
     fstatat64 = __NR_newfstatat,
     ftruncate = __NR_ftruncate,
+#ifdef __NR_arch_prctl
     arch_prctl = __NR_arch_prctl,
+#endif
     brk = __NR_brk,
     exit_group = __NR_exit_group,
     clone3 = __NR_clone3,
@@ -43,12 +45,11 @@ enum class Number : int {
     clock_nanosleep = __NR_clock_nanosleep,
     clock_gettime = __NR_clock_gettime,
     setsid = __NR_setsid,
-    dup2 = __NR_dup2,
+    dup3 = __NR_dup3,
     pipe2 = __NR_pipe2,
     chdir = __NR_chdir,
     fchdir = __NR_fchdir,
-    rmdir = __NR_rmdir,
-    unlink = __NR_unlink,
+    unlinkat = __NR_unlinkat,
     uname = __NR_uname,
 };
 
@@ -65,11 +66,9 @@ namespace detail {
 
 template<detail::SystemCallResult R>
 auto system_call(Number number) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res)
-                 : DIUS_SYSTEM_CALL_CLOBBER);
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION : "=r"(res) : "r"(num) : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
     }
@@ -78,13 +77,10 @@ auto system_call(Number number) -> di::Expected<R, di::BasicError> {
 
 template<detail::SystemCallResult R, detail::SystemCallArgument T1>
 auto system_call(Number number, T1&& a1) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    auto y1 = SystemCallArg(a1);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1)
-                 : DIUS_SYSTEM_CALL_CLOBBER);
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION : "=r"(res) : "r"(num), "r"(x1) : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
     }
@@ -93,15 +89,11 @@ auto system_call(Number number, T1&& a1) -> di::Expected<R, di::BasicError> {
 
 template<detail::SystemCallResult R, detail::SystemCallArgument T1, detail::SystemCallArgument T2>
 auto system_call(Number number, T1&& a1, T2&& a2) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    auto y1 = SystemCallArg(a1);
-    auto y2 = SystemCallArg(a2);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    register SystemCallArg x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = y2;
-    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1), "r"(x2)
-                 : DIUS_SYSTEM_CALL_CLOBBER);
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    auto register x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = SystemCallArg(a2);
+    asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION : "=r"(res) : "r"(num), "r"(x1), "r"(x2) : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
     }
@@ -111,16 +103,14 @@ auto system_call(Number number, T1&& a1, T2&& a2) -> di::Expected<R, di::BasicEr
 template<detail::SystemCallResult R, detail::SystemCallArgument T1, detail::SystemCallArgument T2,
          detail::SystemCallArgument T3>
 auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    auto y1 = SystemCallArg(a1);
-    auto y2 = SystemCallArg(a2);
-    auto y3 = SystemCallArg(a3);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    register SystemCallArg x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = y2;
-    register SystemCallArg x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = y3;
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    auto register x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = SystemCallArg(a2);
+    auto register x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = SystemCallArg(a3);
     asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1), "r"(x2), "r"(x3)
+                 : "=r"(res)
+                 : "r"(num), "r"(x1), "r"(x2), "r"(x3)
                  : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
@@ -131,18 +121,15 @@ auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3) -> di::Expected<R, di
 template<detail::SystemCallResult R, detail::SystemCallArgument T1, detail::SystemCallArgument T2,
          detail::SystemCallArgument T3, detail::SystemCallArgument T4>
 auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3, T4&& a4) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    auto y1 = SystemCallArg(a1);
-    auto y2 = SystemCallArg(a2);
-    auto y3 = SystemCallArg(a3);
-    auto y4 = SystemCallArg(a4);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    register SystemCallArg x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = y2;
-    register SystemCallArg x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = y3;
-    register SystemCallArg x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = y4;
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    auto register x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = SystemCallArg(a2);
+    auto register x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = SystemCallArg(a3);
+    auto register x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = SystemCallArg(a4);
     asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1), "r"(x2), "r"(x3), "r"(x4)
+                 : "=r"(res)
+                 : "r"(num), "r"(x1), "r"(x2), "r"(x3), "r"(x4)
                  : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
@@ -153,20 +140,16 @@ auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3, T4&& a4) -> di::Expec
 template<detail::SystemCallResult R, detail::SystemCallArgument T1, detail::SystemCallArgument T2,
          detail::SystemCallArgument T3, detail::SystemCallArgument T4, detail::SystemCallArgument T5>
 auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3, T4&& a4, T5&& a5) -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    SystemCallArg y1 = SystemCallArg(a1);
-    SystemCallArg y2 = SystemCallArg(a2);
-    SystemCallArg y3 = SystemCallArg(a3);
-    SystemCallArg y4 = SystemCallArg(a4);
-    SystemCallArg y5 = SystemCallArg(a5);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    register SystemCallArg x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = y2;
-    register SystemCallArg x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = y3;
-    register SystemCallArg x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = y4;
-    register SystemCallArg x5 asm(DIUS_SYSTEM_CALL_ASM_ARG5) = y5;
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    auto register x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = SystemCallArg(a2);
+    auto register x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = SystemCallArg(a3);
+    auto register x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = SystemCallArg(a4);
+    auto register x5 asm(DIUS_SYSTEM_CALL_ASM_ARG5) = SystemCallArg(a5);
     asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5)
+                 : "=r"(res)
+                 : "r"(num), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5)
                  : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
@@ -179,22 +162,17 @@ template<detail::SystemCallResult R, detail::SystemCallArgument T1, detail::Syst
          detail::SystemCallArgument T6>
 auto system_call(Number number, T1&& a1, T2&& a2, T3&& a3, T4&& a4, T5&& a5, T6&& a6)
     -> di::Expected<R, di::BasicError> {
-    SystemCallResult res = di::to_underlying(number);
-    auto y1 = SystemCallArg(a1);
-    auto y2 = SystemCallArg(a2);
-    auto y3 = SystemCallArg(a3);
-    auto y4 = SystemCallArg(a4);
-    auto y5 = SystemCallArg(a5);
-    auto y6 = SystemCallArg(a6);
-    register SystemCallArg x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = y1;
-    register SystemCallArg x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = y2;
-    register SystemCallArg x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = y3;
-    register SystemCallArg x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = y4;
-    register SystemCallArg x5 asm(DIUS_SYSTEM_CALL_ASM_ARG5) = y5;
-    register SystemCallArg x6 asm(DIUS_SYSTEM_CALL_ASM_ARG6) = y6;
+    auto register res asm(DIUS_SYSTEM_CALL_ASM_RESULT) = SystemCallResult(0);
+    auto register num asm(DIUS_SYSTEM_CALL_ASM_NUMBER) = di::to_underlying(number);
+    auto register x1 asm(DIUS_SYSTEM_CALL_ASM_ARG1) = SystemCallArg(a1);
+    auto register x2 asm(DIUS_SYSTEM_CALL_ASM_ARG2) = SystemCallArg(a2);
+    auto register x3 asm(DIUS_SYSTEM_CALL_ASM_ARG3) = SystemCallArg(a3);
+    auto register x4 asm(DIUS_SYSTEM_CALL_ASM_ARG4) = SystemCallArg(a4);
+    auto register x5 asm(DIUS_SYSTEM_CALL_ASM_ARG5) = SystemCallArg(a5);
+    auto register x6 asm(DIUS_SYSTEM_CALL_ASM_ARG6) = SystemCallArg(a6);
     asm volatile(DIUS_SYSTEM_CALL_INSTRUCTION
-                 : DIUS_SYSTEM_CALL_ASM_RESULT(res)
-                 : DIUS_SYSTEM_CALL_ASM_NUMBER(res), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5), "r"(x6)
+                 : "=r"(res)
+                 : "r"(num), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5), "r"(x6)
                  : DIUS_SYSTEM_CALL_CLOBBER);
     if (res < 0) {
         return di::Unexpected(di::BasicError(-res));
